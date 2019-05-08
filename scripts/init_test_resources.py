a8647bfdc5b3df99434a2b376a7d78af834526d9
Allan Wang <me@allanwang.ca>
2019-05-08 11:13:31 +0800

    Create javap variants

--- scripts/init_test_resources.py
+++ scripts/init_test_resources.py
@@ -14,24 +14,43 @@ def files_dir(*paths):
     return p.abspath(p.join(project_dir, 'files', *paths))
 
 
-java_dir = files_dir('java')
+_java_dir = files_dir('java')
 
-gen_dir = files_dir('gen')
-
-java_files = [f for f in os.listdir(java_dir) if
+java_files = [f for f in os.listdir(_java_dir) if
               f.endswith('.java')]
 
 if not java_files:
     print("No java files found")
     exit(0)
 
-if p.isdir(gen_dir):
-    shutil.rmtree(gen_dir)
+_gen_dir = files_dir('gen')
 
-os.makedirs(gen_dir, exist_ok=True)
+if p.isdir(_gen_dir):
+    shutil.rmtree(_gen_dir)
+
+
+def gen_dir(*paths):
+    path = p.abspath(p.join(_gen_dir, *paths))
+    os.makedirs(path, exist_ok=True)
+    return path
+
+
+javac_dir = gen_dir('javac')
+javap_dir = gen_dir('javap')
+javap_v_dir = gen_dir('javap_v')
 
 for f in java_files:
     name = f[:-5]
-    path = p.join(java_dir, f)
-    subprocess.call(['javac', path, '-d', gen_dir])
-    print(name, path, f)
+    print("Generating {}".format(f))
+    path = p.join(_java_dir, f)
+    subprocess.call(['javac', path, '-d', javac_dir])
+    javac_path = p.join(javac_dir, name + '.class')
+    with open(p.join(javap_dir, name + ".j"), 'w+') as p_file:
+        subprocess.call(
+            ['javap', '-p', '-c', javac_path],
+            stdout=p_file)
+
+    with open(p.join(javap_v_dir, name + ".j"), 'w+') as p_file:
+        subprocess.call(
+            ['javap', '-p', '-c', '-v', javac_path],
+            stdout=p_file)
