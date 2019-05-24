998ddfc60fda25ed1b91d01e13c3d2557a9bb364
Julian Lore <julian.lore@mail.mcgill.ca>
2019-05-24 00:42:48 -0400

    Add command line parsing with placeholder

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
 
-import           Lib
+import qualified Options.Applicative as Op
+import           ParseCLI
 
 main :: IO ()
-main = someFunc
+main = do
+  (CI cmd f) <-
+    Op.customExecParser (Op.prefs Op.showHelpOnEmpty) cmdParser
+  readFile f >>=
+    case cmd of
+      _ -> putStrLn
